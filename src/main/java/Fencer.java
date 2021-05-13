08389648c14bd438a7695bab6eda143944353b39
Reevak05 <kaveergera@gmail.com>
2021-05-13 10:29:16 -0700

    Add name field, .equals(), and .toString to Fencer

--- src/main/java/Fencer.java
+++ src/main/java/Fencer.java
@@ -1,13 +1,31 @@
+import java.util.HashMap;
+
 public class Fencer {
 
     private final int number;
+    private final String name;
 
-    public Fencer(int number) {
+    public Fencer(int number, String name) {
         this.number = number;
+        this.name = name;
     }
 
     public int getNumber() {
         return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public boolean equals(Fencer otherFencer) {
+        return (this.number == otherFencer.getNumber());
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + this.name + ", number: " + this.number;
     }
 
 
