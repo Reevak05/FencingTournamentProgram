d0e44ab9d57551a5262b82a987ef2f6b5e0f51b3
Reevak05 <kaveergera@gmail.com>
2021-05-12 23:54:55 -0700

    initial commit; add FencingTournamentProgram class

---
+++ src/main/java/FencingTournamentProgram.java
@@ -0,0 +1,10 @@
+public class FencingTournamentProgram {
+
+    public FencingTournamentProgram() {
+        System.out.println("FencingTournamentProgram");
+    }
+
+    public static void main(String[] args) {
+        FencingTournamentProgram fencingTournamentProgram = new FencingTournamentProgram();
+    }
+}
